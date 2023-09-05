b1accea01c11aa010c771e1b2225f829656ddba0
Diego Benavides <diego4bits@proton.me>
2023-09-05 11:58:46 -0500

    hotfix of code snippet string

--- src/snippet_management/code_snippet.py
+++ src/snippet_management/code_snippet.py
@@ -42,7 +42,7 @@ class CodeSnippet:
         )
 
     def __str__(self):
-        string = f"\n\nfile_path: {self.file_path}\nfile_name: {self.file_name}\nfile_extension: {self.file_extension}\ndependencies:\n{self.dependencies}\nsnippet_type: {self.code_snippet_type}\nsnippet_identifier: {self.code_snippet_identifier}\nsnippet_implementation:\n\n{self.code_snippet_implementation}\n"
+        string = f"\n\nfile_path: {self.file_path}\nfile_name: {self.file_name}\nfile_extension: {self.file_extension}\ndependencies:\n{self.dependencies}\n\nsnippet_type: {self.code_snippet_type}\nsnippet_identifier: {self.code_snippet_identifier}\nsnippet_implementation:\n\n{self.code_snippet_implementation}\n"
         return string
 
     @property
